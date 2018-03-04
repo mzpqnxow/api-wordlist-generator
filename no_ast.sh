514d56fbf69d87473ffa7bd9df89fac341b2931b
Adam Greene <github@mzpqnxow.com>
2018-03-04 12:14:56 -0500

    uniq

--- no_ast.sh
+++ no_ast.sh
@@ -5,5 +5,4 @@
 # Copyright 2018, 3-clause BSD license, copyright /at/ mzpqnxow.com
 #
 PATH_TO_PYTHON_CODE=venv/
-find "$PATH_TO_PYTHON_CODE" -name \*.py -exec grep -Eo ' *def (.*) *\(' {} \; | sed -e 's/^ *def *//g' | cut -d '(' -f 1 | grep -v '^_' | grep -E -v '[=,\{}]' | sort -u > funcs.lst
-
+find "$PATH_TO_PYTHON_CODE" -name \*.py -exec grep -Eo ' *def (.*) *\(' {} \; | sed -e 's/^ *def *//g' | cut -d '(' -f 1 | grep -v '^_' | grep -E -v '[=,\{}]' | sort -u
