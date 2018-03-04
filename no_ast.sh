1cf22c5bed3396e6a67619f46edd68d82df7646e
Adam Greene <github@mzpqnxow.com>
2018-03-04 12:12:41 -0500

    .

---
+++ no_ast.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Extract all method names from a directory containing Python code (deep search)
+#
+# Copyright 2018, 3-clause BSD license, copyright /at/ mzpqnxow.com
+#
+PATH_TO_PYTHON_CODE=venv/
+find "$PATH_TO_PYTHON_CODE" -name \*.py -exec grep -Eo ' *def (.*) *\(' {} \; | sed -e 's/^ *def *//g' | cut -d '(' -f 1 | grep -v '^_' | grep -E -v '[=,\{}]' > funcs.lst
+
